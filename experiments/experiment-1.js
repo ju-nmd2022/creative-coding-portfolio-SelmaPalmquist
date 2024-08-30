8880468fbe693dda908f0f949b385f8aaeb4c377
SelmaPalmquist <pase22pc@student.ju.se>
2024-08-30 15:16:42 +0200

    Start week 1

---
+++ experiments/experiment-1.js
@@ -0,0 +1,7 @@
+[
+    {
+      "file": "experiments/experiment-1.js",
+      "name": "Experiment Week 1",
+      "description": "Explains the what and the why. What is the main idea? What is the purpose? Describes the coding process and implementation choices and gives a high level system overview."
+    }
+  ]
