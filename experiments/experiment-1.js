663cbcf091d4f6cc84c0c0bdda3c4e3a117afc96
SelmaPalmquist <pase22pc@student.ju.se>
2024-09-02 16:55:59 +0200

    Test on lab 1

--- experiments/experiment-1.js
+++ experiments/experiment-1.js
@@ -1,7 +1,34 @@
-[
-    {
-      "file": "experiments/experiment-1.js",
-      "name": "Experiment Week 1",
-      "description": "Explains the what and the why. What is the main idea? What is the purpose? Describes the coding process and implementation choices and gives a high level system overview."
+let gap = 2;
+let stripeWidth = 3;
+let amount = 1000;
+
+
+function setup (){
+    createCanvas(innerWidth, innerHeight);
+    background(247,241,193);
+    frameRate(8);
+}
+
+function shapes (){
+  rotate(PI / random(0, 3));
+  rect(random(0, 600), (random(0, 600)), random(10, 40));
+
+}
+
+function draw(){
+  noStroke();
+
+  for(let y = 0; y < 1; y++) {
+    for (let x = 0; x < amount; x++) {
+      fill(random(0, 255), random(0, 255), random(0, 255));
+    rect(x,y,stripeWidth,innerHeight);
+    x += stripeWidth + gap;
     }
-  ]
+    y += stripeWidth + gap;
+
+    //triangle(random(600), random(600), 0, 0, 10, 5);
+    shapes();
+}
+}
+
+
