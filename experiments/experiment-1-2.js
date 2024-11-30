73fb08f1bb5a12d129ee2d9681064c92d02d04a0
SelmaPalmquist <pase22pc@student.ju.se>
2024-11-30 21:37:46 +0100

    color and easier opacity gradient

--- experiments/experiment-1-2.js
+++ experiments/experiment-1-2.js
@@ -1,8 +1,9 @@
 //followed video provided by Garrit Shaap on perlin noise
 //https://play.ju.se/media/Noise+examples%2C+and+Vera+Moln%C3%A1r/0_3pcpvm3q
-//and the gradual opacity change was created based on https://p5js.org/reference/p5.Color/setAlpha/ 
+//and the gradual color change was created based on https://www.youtube.com/watch?v=-MUOweQ6wac&t=60s 
 function setup() {
   createCanvas(600, 600);
+  colorMode(HSB, 360, 100, 100, 100);
   frameRate(4);
 }
 
@@ -12,22 +13,22 @@ const numRows = 60;
 const numCols = 60;
 
 let counter = 0;
-let c = color(255, 0, 0);
 
 function draw() {
   background(255);
   noStroke();
 
+  let gradient = drawingContext.createLinearGradient(0, 0, 0, height);
+
+  gradient.addColorStop(0, color(56, 100, 100, 0)); 
+  gradient.addColorStop(1, color(0, 100, 100, 100));
+  drawingContext.fillStyle = gradient;
+
   for (let y = 0; y < numRows; y++) {
     for (let x = 0; x < numCols; x++) {
     
       const value = noise(x / divider, y / divider, counter) * size;
-     
-      //changes opacity gradually. The first row here was completed with the help of ChatGPT
-      const alpha = map(y, 10, numRows - 1, 0, 255);
-      c.setAlpha(alpha);
-
-      fill(c);
+   
       ellipse(size / 2 + x * size, size / 2 + y * size, value);
       
     }
