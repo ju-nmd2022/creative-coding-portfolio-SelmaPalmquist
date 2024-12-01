fb94249ce94b94e0b75ecf63f82b4c8c368eba92
SelmaPalmquist <pase22pc@student.ju.se>
2024-12-01 04:23:23 +0100

    Flow field from complexity with some tweeks

--- experiments/experiment-3-1.js
+++ experiments/experiment-3-1.js
@@ -1,33 +1,116 @@
-//followed video provided by Garrit Shaap on perlin noise
-//https://play.ju.se/media/Noise+examples%2C+and+Vera+Moln%C3%A1r/0_3pcpvm3q
-//flow fields will go here? version 1
-function setup() {
-    createCanvas(600, 600);
+//base code copied from the Complexity lesson https://ju.slides.com/garrit/cc2024-complexity?token=Nl2_bLqJ#/9/6
+class Agent {
+  constructor(x, y, maxSpeed, maxForce) {
+    this.position = createVector(x, y);
+    this.lastPosition = createVector(x, y);
+    this.acceleration = createVector(0, 0);
+    this.velocity = createVector(0, 0);
+    this.maxSpeed = maxSpeed;
+    this.maxForce = maxForce;
   }
-  
-  const size = 10;
-  const divider = 20;
-  const numRows = 60;
-  const numCols = 60;
-  
-  function draw() {
-    background(255);
-    noStroke();
-  
-    for (let y = 0; y < numRows; y++) {
-      for (let x = 0; x < numCols; x++) {
-        const noiseValue = noise(x / divider, y / divider);
-        const value = noiseValue * size;
-        
-        const r = random(0, 255);
-        const g = random(0, 255);
-        const b = random(0, 255);
-  
-        fill(r, g, b);
-        ellipse(size / 2 + x * size, size / 2 + y * size, value);
-      }
+
+  follow(desiredDirection) {
+    desiredDirection = desiredDirection.copy();
+    desiredDirection.mult(this.maxSpeed);
+    let steer = p5.Vector.sub(desiredDirection, this.velocity);
+    steer.limit(this.maxForce);
+    this.applyForce(steer);
+  }
+
+  applyForce(force) {
+    this.acceleration.add(force);
+  }
+
+  update() {
+    this.lastPosition = this.position.copy();
+    this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.position.add(this.velocity);
+    this.acceleration.mult(0);
+  }
+
+  checkBorders() {
+    if (this.position.x < 0) {
+      this.position.x = innerWidth;
+      this.lastPosition.x = innerWidth;
+    } else if (this.position.x > innerWidth) {
+      this.position.x = 0;
+      this.lastPosition.x = 0;
     }
-    noLoop();
+    if (this.position.y < 0) {
+      this.position.y = innerHeight;
+      this.lastPosition.y = innerHeight;
+    } else if (this.position.y > innerHeight) {
+      this.position.y = 0;
+      this.lastPosition.y = 0;
+    }
   }
-  
+
+  draw() {
+    push();
+    
+    stroke(40, 200, 200, 40);
+    strokeWeight(2);
+    line(
+      this.lastPosition.x,
+      this.lastPosition.y,
+      this.position.x,
+      this.position.y
+    );
+    pop();
+  }
+}
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  background(255, 255, 255);
+  field = generateField();
+  generateAgents();
+}
+
+function generateField() {
+  let field = [];
+  noiseSeed(Math.random() * 100);
+  for (let x = 0; x < maxCols; x++) {
+    field.push([]);
+    for (let y = 0; y < maxRows; y++) {
+      const value = noise(x / divider, y / divider) * Math.PI * 2;
+      field[x].push(p5.Vector.fromAngle(value));
+    }
+  }
+  return field;
+}
+
+function generateAgents() {
+  for (let i = 0; i < 200; i++) {
+    let agent = new Agent(
+      Math.random() * innerWidth,
+      Math.random() * innerHeight,
+      2,
+      0.5
+    );
+    agents.push(agent);
+  }
+}
+
+const fieldSize = 50;
+const maxCols = Math.ceil(innerWidth / fieldSize);
+const maxRows = Math.ceil(innerHeight / fieldSize);
+const divider = 2;
+let field;
+let agents = [];
+
+function draw() {
+  for (let agent of agents) {
+    const x = Math.floor(agent.position.x / fieldSize);
+    const y = Math.floor(agent.position.y / fieldSize);
+    const desiredDirection = field[x][y];
+    agent.follow(desiredDirection);
+    agent.update();
+    agent.checkBorders();
+    agent.draw();
+  }
+}
+
+
 
