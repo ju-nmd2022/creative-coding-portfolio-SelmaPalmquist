31130bfa24078f312565e125af1834042dfc6da5
SelmaPalmquist <pase22pc@student.ju.se>
2024-12-01 03:48:46 +0100

    begin on tone

--- experiments/experiment-4-1.js
+++ experiments/experiment-4-1.js
@@ -1,2 +1,30 @@
-//tone.js will go here.
-
+//tone.js will go here. the idea is to make the sound change based on shape and size
+function setup() {
+    createCanvas(1000, 1000);
+    background(0);
+    frameRate(1);
+  }
+  
+  function drawShapes() {
+    noStroke();
+    //randomized position and size for each ellipse
+    let xPosition = random(0, 900); 
+    let yPosition = random(0, 900); 
+    let size = random(10, 150); 
+  
+    //randomized color for each ellipse
+    fill(random(100, 255), random(100, 255), random(100, 255));
+  
+    let shape = Math.floor(Math.random() * 3);
+    if(shape === 0){
+        rect(xPosition, yPosition, size, size);
+    } else {
+        ellipse(xPosition, yPosition, size, size);
+    }
+  }
+  
+  function draw() {
+    drawShapes();
+    }
+  
+  
