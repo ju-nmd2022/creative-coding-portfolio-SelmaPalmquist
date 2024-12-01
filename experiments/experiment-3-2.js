a13594e3bdea83b199f53d448ceb9566620b25fd
SelmaPalmquist <pase22pc@student.ju.se>
2024-12-01 23:27:10 +0100

    version 2 of flow fields

--- experiments/experiment-3-2.js
+++ experiments/experiment-3-2.js
@@ -1,5 +1,136 @@
-//flow fields will go here? https://www.youtube.com/watch?time_continue=745&v=R0OFyWEglGA&embeds_referring_euri=https%3A%2F%2Fwww.google.com%2Fsearch%3Fq%3Dhow%2Bto%2Bmake%2Bflow%2Bfields%2Bdraw%2Barcs%2Binstead%2Bof%2Blines%2Bp5%26sca_esv%3Df9439907e5543505%26sxsrf%3DADLYWILSdCm&source_ve_path=MTM5MTE3LDEzOTExNywxMjcyOTksMTM5MTE3LDEzOTExNywxMzkxMTcsMTM5MTE3LDEzOTExNywxMzkxMTcsMTM5MTE3LDEzOTExNywxMzkxMTcsMjM4NTE
-function setup() {
+//base code copied from the Complexity lesson https://ju.slides.com/garrit/cc2024-complexity?token=Nl2_bLqJ#/9/6
+class Agent {
+    constructor(x, y, maxSpeed, maxForce) {
+      this.position = createVector(x, y);
+      this.lastPosition = createVector(x, y);
+      this.acceleration = createVector(0, 0);
+      this.velocity = createVector(0, 0);
+      this.maxSpeed = maxSpeed;
+      this.maxForce = maxForce;
+
+      //randomize colors from selected colors that are similar to the inspo picture
+      let c = Math.floor(Math.random() * 6);
+
+      if (c === 0) {
+        this.color = color(105, 100, 100, 100);
+      } else if (c === 1) {
+        this.color = color(56, 100, 100, 100);
+      } else if (c === 2) {
+        this.color = color(10, 100, 100, 100);
+      } else if (c === 3) {
+        this.color = color(254, 88, 100, 100);
+      } else if (c === 4) {
+        this.color = color(184, 100, 100, 100);
+      } else {
+        this.color = color(309, 100, 100, 100);
+      }
+    }
+  
+    follow(desiredDirection) {
+      desiredDirection = desiredDirection.copy();
+      desiredDirection.mult(this.maxSpeed);
+      let steer = p5.Vector.sub(desiredDirection, this.velocity);
+      steer.limit(this.maxForce);
+      this.applyForce(steer);
+    }
+  
+    applyForce(force) {
+      this.acceleration.add(force);
+    }
+  
+    update() {
+      this.lastPosition = this.position.copy();
+      this.velocity.add(this.acceleration);
+      this.velocity.limit(this.maxSpeed);
+      this.position.add(this.velocity);
+      this.acceleration.mult(0);
+    }
+  
+    checkBorders() {
+      if (this.position.x < 0) {
+        this.position.x = innerWidth;
+        this.lastPosition.x = innerWidth;
+      } else if (this.position.x > innerWidth) {
+        this.position.x = 0;
+        this.lastPosition.x = 0;
+      }
+      if (this.position.y < 0) {
+        this.position.y = innerHeight;
+        this.lastPosition.y = innerHeight;
+      } else if (this.position.y > innerHeight) {
+        this.position.y = 0;
+        this.lastPosition.y = 0;
+      }
+    }
+  
+    draw() {
+      push();
+      stroke(this.color);
+      strokeWeight(5);
+      line(
+        this.lastPosition.x,
+        this.lastPosition.y,
+        this.position.x,
+        this.position.y
+      );
+      pop();
+    }
+  }
+  
+  function setup() {
+    createCanvas(innerWidth, innerHeight);
+    background(255);
+    colorMode(HSB, 360, 100, 100, 100);
+    field = generateField();
+    generateAgents();
+  }
+  
+  function generateField() {
+    let field = [];
+    noiseSeed(Math.random() * 100);
+    for (let x = 0; x < maxCols; x++) {
+      field.push([]);
+      for (let y = 0; y < maxRows; y++) {
+        const value = noise(x / divider, y / divider) * Math.PI * 2;
+        field[x].push(p5.Vector.fromAngle(value));
+      }
+    }
+    return field;
+  }
+  
+  function generateAgents() {
+    for (let i = 0; i < 200; i++) {
+      let agent = new Agent(
+        Math.random() * innerWidth,
+        Math.random() * innerHeight,
+        4,
+        0.3
+      );
+      agents.push(agent);
+    }
+  }
+  
+  const fieldSize = 50;
+  const maxCols = Math.ceil(innerWidth / fieldSize);
+  const maxRows = Math.ceil(innerHeight / fieldSize);
+  const divider = 4;
+  let field;
+  let agents = [];
+  
+  function draw() {
+    for (let agent of agents) {
+      const x = Math.floor(agent.position.x / fieldSize);
+      const y = Math.floor(agent.position.y / fieldSize);
+      const desiredDirection = field[x][y];
+      agent.follow(desiredDirection);
+      agent.update();
+      agent.checkBorders();
+      agent.draw();
+    }
+  }
+
+
+/*function setup() {
 createCanvas(1000,1000);
 colorMode(HSB, 360, 120, 100, 25);
 background(0);
@@ -29,4 +160,4 @@ for (i=0; i < width; i += gap){
     } 
 }
 }
-
+*/
