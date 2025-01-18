8ae08f88fb5e424bd6124af338d8fe7606123327
SelmaPalmquist <pase22pc@student.ju.se>
2025-01-18 16:31:18 +0100

    trying tone.js

--- experiments/experiment-5-1.js
+++ experiments/experiment-5-1.js
@@ -1,18 +1,38 @@
+//this is what i tried that doenst work
 //tone.js will go here. the idea is to make the sound change based on shape and size
+const synth = new Tone.Synth().toDestination();
+
+
+
+
 function setup() {
-    createCanvas(1000, 1000);
+    createCanvas(600, 600);
     background(0);
-    frameRate(1);
+    frameRate(0.01);
+    fill(255,255,255);
+    rect(200, 200, 200, 100);
+}
+
+let isToneStarted = false;
+
+
+
+function mousePressed() {
+    if (mouseX > 200 && mouseX < 400 && mouseY > 200 && mouseY < 300) {
+      isToneStarted = true;
+      console.log('Tone.js started!');
+      Tone.start();
+    }
   }
-  
+
   function drawShapes() {
     noStroke();
-    //randomized position and size for each ellipse
-    let xPosition = random(0, 900); 
-    let yPosition = random(0, 900); 
+    //randomized position and size for each shape
+    let xPosition = random(0, 500); 
+    let yPosition = random(0, 500); 
     let size = random(10, 150); 
   
-    //randomized color for each ellipse
+    //randomized color for each shape
     fill(random(100, 255), random(100, 255), random(100, 255));
   
     let shape = Math.floor(Math.random() * 3);
@@ -24,6 +44,31 @@ function setup() {
   }
   
   function draw() {
-    drawShapes();
+    if (isToneStarted === true) {
+        background(0);
+        drawShapes();
+      } else {
+        console.log("Tone didnt start.")
+      }
     }
+
+
+
+    
+
+
+
+    /* 
+    playButton.addEventListener("click", () => {
+        if(Tone.context.state !== "running"){
+            Tone.start();
+        }
+        synth.triggerAttackRelease("C3", "8n");
+    });
   
+    console.log(Tone);
+
+
+let oscillator = new Tone.Oscillator(440, "sine")
+.toDestination().start();
+*/
